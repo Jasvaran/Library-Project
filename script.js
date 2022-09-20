e3e4946d18c93c876006be17f08877c96f042c16
Jasvaran Mann <mannjesse355@gmail.com>
2022-09-20 13:52:33 -0700

    Created book class and function to render each book

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let libraryModule = (function () {
 
+    const bookTableBody = document.querySelector('.book-table-body');
+
     let bookArray = [
         {
             title: 'Jesse',
@@ -16,6 +18,31 @@ let libraryModule = (function () {
         }
     ]
 
-    
+    class BookClass {
+        constructor (title, author, pages, read) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.read = read;
+        }
+    }
+
+    console.log(bookArray[0].title)
+
+    let renderModule = (function() {
+        bookArray.forEach(book => {
+          const bookHtml = `
+            <tr>
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.pages}</td>
+                <td>${book.read}</td>
+            </tr>
+          `
+          bookTableBody.insertAdjacentHTML("beforeend", bookHtml)
+        });
+    })();
+
+    return {bookArray, BookClass, renderModule}
 
 })();
