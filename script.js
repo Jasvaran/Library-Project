d93fe3114ca9583e05f5cb9daf731068dab6450c
Jasvaran Mann <mannjesse355@gmail.com>
2022-09-20 00:16:09 -0700

    Added sidebar image, grid, table

---
+++ script.js
@@ -0,0 +1,21 @@
+let libraryModule = (function () {
+
+    let bookArray = [
+        {
+            title: 'Jesse',
+            author: 'Mann',
+            pages: 10,
+            read: false,
+        },
+
+        {
+            title: 'Mano',
+            author: 'Toor',
+            pages: 10,
+            read: false
+        }
+    ]
+
+    
+
+})();
